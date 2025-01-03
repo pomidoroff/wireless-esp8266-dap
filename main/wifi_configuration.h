7628a07d0f79e5de784800999f1aa3fb6ec0ead3
pomidoroff <dman@tut.by>
2025-01-03 09:48:05 +0300

    Update wifi_configuration.h

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -15,7 +15,7 @@ static struct {
     const char *ssid;
     const char *password;
 } wifi_list[] __attribute__((unused)) = {
-    {.ssid = "OTA", .password = "12345678"},
+    {.ssid = "Eton_kb", .password = "Battlefield2"},
     {.ssid = "DAP", .password = "12345678"},
     // Add your WAP like this:
     // {.ssid = "your ssid", .password = "your password"},
